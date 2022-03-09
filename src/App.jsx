56f62017a0d73e12a2e24a8e8ce8ee19cace331a
budou114 <58349030+budou114@users.noreply.github.com>
2022-03-09 14:43:52 +0900

    use effectを知る

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorfullMessage from './components/ColorfullMessage';
 
 const App = () => {
+  console.log('最初');
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+
   const onClickCountUp = () => {
     setNum(num + 1);
   };
-  const [num, setNum] = useState(0);
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    // console.log('useEffect!!');
+
+    if (num > 0 && num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
 
 	return (
     <>
@@ -17,7 +34,10 @@ const App = () => {
         元気です
       </ColorfullMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(^^)/</p>}
     </>
   );
 };
--- src/components/ColorfullMessage.jsx
+++ src/components/ColorfullMessage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ColorhullMessage = (props) => {
+  console.log('カラフル');
 	const {color, children} = props;
   const contenStyle = {
     color,
